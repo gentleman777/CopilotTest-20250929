8eae355640be0c128c01e031164360cc7ce397ce
gentleman777 <cjkim777@gmail.com>
2025-09-29 20:27:19 +0900

    주석 추가

--- src/main/java/com/obj/meeting/controller/MeetingController.java
+++ src/main/java/com/obj/meeting/controller/MeetingController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+// Test Commit
+
 @Controller
 public class MeetingController {
 
